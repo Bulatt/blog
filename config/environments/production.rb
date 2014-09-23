6afd99f8046da12ea60a84801e68e02767b37d92
Bulat Abyanov <boolmc@yandex.ru>
2014-09-23 21:07:55 +0400

    Change config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,21 +64,23 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'http://abynov.herokuapp.com/', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'http://abynov.herokuapp.com', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   # change to true to allow email to be sent during development
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "http://abynov.herokuapp.com/",
+    domain: 'heroku.com',
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
   }
+
+  Rails.application.routes.default_url_options[:host] = 'http://abynov.herokuapp.com'
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
